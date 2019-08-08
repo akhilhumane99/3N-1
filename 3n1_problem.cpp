5d2073c980ea9ea9333ba4f5ecfa188089da3822
Akhil Humane <45690913+akhilhumane99@users.noreply.github.com>
2019-08-08 11:44:46 -0700

    Add files via upload

---
+++ 3n1_problem.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<iostream>
+
+/* Akhil Humane
+PRN : 10303320171124510016*/
+
+using namespace std;
+
+int main()
+{   unsigned int n;
+	int i,x,y,j,temp,max_count=0,count=0;
+ cin.tie(NULL);
+	while(scanf("%d %d",&i,&j)!=EOF){
+	x=i,y=j;
+	if(i>j)
+	{ temp=i;
+	  i=j;
+	  j=temp;
+	}
+	
+    max_count=0;
+	while(i<=j)
+	{ n=i;
+	count=1;
+	while(n!=1)
+	 {
+	 	if(n%2==1)
+	 	 n=3*n+1;
+	 	else
+	 	 n=n/2;
+	 	count++;
+	 }
+		 if(max_count<count)
+	 	  max_count=count; 
+    i++;
+	}
+printf("%d %d %d \n",x,y,max_count);}
+return 0;
+}
+	
+	
+
